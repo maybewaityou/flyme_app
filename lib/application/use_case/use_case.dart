6b489ff34968238660659543740504ba381f1ed3
MeePwn <dong.chunnan@chinamworld.com>
2020-05-28 15:58:29 +0800

    架构分层

--- lib/application/use_case/use_case.dart
+++ lib/application/use_case/use_case.dart
@@ -1,14 +1,1 @@
-import 'package:flyme_app/domain/entity/entity.dart';
-
 /// 业务用例
-
-class LoginUseCase {
-  final Login _login;
-  const LoginUseCase(this._login);
-
-  // 业务用例
-  void login(params) {
-    // TODO params
-    _login.login();
-  }
-}
--- lib/infrastructure/repository/repository.dart
+++ lib/infrastructure/repository/repository.dart
@@ -1,9 +1,1 @@
 /// 持久层 - 实现
-import 'package:flyme_app/infrastructure/model/model.dart';
-
-void test() {
-  final user = User("userName", 20, Dog("wangcia"));
-  user.name;
-  user.dog.name;
-  user.age;
-}

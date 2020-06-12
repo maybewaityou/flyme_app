a65c92e58c317b9e4feeae0d9c483278b4a5d90d
MeePwn <dong.chunnan@chinamworld.com>
2020-06-12 18:41:21 +0800

    架构分层

--- lib/user_interface/global/locale_model.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'locale_model.dart';
-
-// **************************************************************************
-// ViewModelGenerator
-// **************************************************************************
-
-class _$ViewModel extends ViewModel {}

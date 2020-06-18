4adb4be45ec2bb59b70ae0372833a6117507b22c
MeePwn <dong.chunnan@chinamworld.com>
2020-06-18 08:31:38 +0800

    first commit

--- lib/feature/registry/domain/model/value_object/registry_info.dart
+++ lib/feature/registry/domain/model/value_object/registry_info.dart
@@ -9,12 +9,57 @@ enum RegistryType {
   weiXin,
 }
 
-class RegistryInfo extends ValueObject<RegistryType> {
-  const RegistryInfo._(this.value);
+class UserName extends ValueObject<String> {
+  const UserName._(this.value);
 
-  factory RegistryInfo({@required RegistryType type}) {
+  factory UserName({@required String input}) => UserName._(
+        validateUserName(input),
+      );
+
+  @override
+  final Either<ValueFailure<String>, String> value;
+}
+
+class EmailAddress extends ValueObject<String> {
+  const EmailAddress._(this.value);
+
+  factory EmailAddress({@required String input}) => EmailAddress._(
+        validateEmailAddress(input),
+      );
+
+  @override
+  final Either<ValueFailure<String>, String> value;
+}
+
+class PhoneNumber extends ValueObject<String> {
+  const PhoneNumber._(this.value);
+
+  factory PhoneNumber({@required String input}) => PhoneNumber._(
+        validatePhoneNumber(input),
+      );
+
+  @override
+  final Either<ValueFailure<String>, String> value;
+}
+
+class RegistryInfo extends ValueObject<RegistryType> {
+  final Either<ValueFailure<String>, String> userName;
+  final Either<ValueFailure<String>, String> email;
+  final Either<ValueFailure<String>, String> phoneNumber;
+
+  RegistryInfo._(this.value, this.userName, this.email, this.phoneNumber);
+
+  factory RegistryInfo({
+    @required RegistryType type,
+    String userName,
+    String email,
+    String phoneNumber,
+  }) {
     return RegistryInfo._(
       validateRegistryType(type),
+      validateUserName(userName),
+      validateEmailAddress(email),
+      validatePhoneNumber(phoneNumber),
     );
   }
 

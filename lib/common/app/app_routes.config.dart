75868ad5456136496929ed59c0104fd70c77e323
MeePwn <dong.chunnan@chinamworld.com>
2020-07-13 12:07:15 +0800

    first commit

--- lib/common/app/app_routes.config.dart
+++ lib/common/app/app_routes.config.dart
@@ -1,10 +1,16 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'app_routes.dart';
-
 // **************************************************************************
 // Generator: Instance of 'RouteConfigGenerator'
 // **************************************************************************
+
+import 'package:fluro/fluro.dart';
+import 'package:flyme_app/common/component/page/not_found_page.dart';
+import 'package:flyme_app/user_interface/splash/splash_page.dart';
+import 'package:flyme_app/user_interface/tab/tab_container.dart';
+import 'package:flyme_app/feature/home/page/second.dart';
+import 'package:flyme_app/feature/auth/user_interface/page/auth_page.dart';
+import 'package:flyme_app/feature/registry/user_interface/page/registry_page.dart';
 
 class AppRoutesPath {
   static const splash = '/splash';
--- lib/common/app/app_routes.dart
+++ lib/common/app/app_routes.dart
@@ -1,13 +1,8 @@
 import 'package:fluro/fluro.dart';
 import 'package:flyme_annotation/flyme_annotation.dart';
-import 'package:flyme_app/common/component/page/not_found_page.dart';
-import 'package:flyme_app/feature/auth/user_interface/page/page.dart';
-import 'package:flyme_app/feature/home/page/second.dart';
-import 'package:flyme_app/feature/registry/user_interface/page/page.dart';
-import 'package:flyme_app/user_interface/splash/splash_page.dart';
-import 'package:flyme_app/user_interface/tab/tab_container.dart';
+import 'package:flyme_app/common/app/app_routes.config.dart';
 
-part 'app_routes.g.dart';
+export 'app_routes.config.dart';
 
 @FRouteConfig()
 class AppRoutes {

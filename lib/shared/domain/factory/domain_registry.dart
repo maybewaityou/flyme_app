813f8359f55e761c403317e1ffa31f086fe8a8cb
MeePwn <dong.chunnan@chinamworld.com>
2020-07-13 11:57:54 +0800

    first commit

--- lib/shared/domain/factory/domain_registry.dart
+++ lib/shared/domain/factory/domain_registry.dart
@@ -1,13 +1,9 @@
 import 'package:flyme_annotation/flyme_annotation.dart';
-import 'package:flyme_app/common/app/application.dart';
-import 'package:flyme_app/feature/auth/domain/repository/auth_repository.dart';
-import 'package:flyme_app/feature/auth/domain/service/service.dart';
-import 'package:flyme_ddd/flyme_ddd.dart';
 
-part 'domain_registry.g.dart';
+import './domain_registry.registry.dart';
 
 @domainFactory
-class DomainRegistry with _$DomainRegistry {
+class DomainRegistry with $DomainRegistry {
   const DomainRegistry._();
 
   factory DomainRegistry.instance() => DomainRegistry._();
--- lib/shared/domain/factory/domain_registry.registry.dart
+++ lib/shared/domain/factory/domain_registry.registry.dart
@@ -1,12 +1,15 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'domain_registry.dart';
-
 // **************************************************************************
 // DomainRegistryGenerator
 // **************************************************************************
 
-class _$DomainRegistry implements IDomainRegistry {
+import 'package:flyme_ddd/flyme_ddd.dart';
+import 'package:flyme_app/common/app/application.dart';
+import 'package:flyme_app/feature/auth/domain/repository/repository.dart';
+import 'package:flyme_app/feature/auth/domain/service/authentication_service.dart';
+
+class $DomainRegistry implements IDomainRegistry {
   IAuthRepository authRepository() => getIt.get();
   AuthenticationService authenticationService() => getIt.get();
 }

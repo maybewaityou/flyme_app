7af78609167e9167418e73c70c0035321d983b60
MeePwn <dong.chunnan@chinamworld.com>
2020-07-31 16:11:05 +0800

    first commit

--- lib/shared/infrastructure/anticorruption/translator/translator.dart
+++ lib/shared/infrastructure/anticorruption/translator/translator.dart
@@ -1,12 +1,1 @@
-import 'package:flyme_app/shared/infrastructure/model/model.dart';
-import 'package:flyme_ddd/flyme_ddd.dart';
 
-class UserInfoTranslator extends DataModelTranslator<UserInfo> {
-  const UserInfoTranslator._();
-  factory UserInfoTranslator() => UserInfoTranslator._();
-
-  @override
-  UserInfo translate(Map<String, dynamic> json) {
-    return UserInfo.fromJson(json);
-  }
-}
